c2c8ef4a068d9da54fd53f63550375de353970e0
cc <votaryofturms@icloud.com>
2022-02-11 02:52:57 -0500

    wip

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
+const server = express()
+
+const PORT = process.env.PORT || 9000
+
+server.use(express.json())
+server.use(cors())
+
+server.get('/api/hello', (req, res) => {
+    res.send('<h1>Hey!</h1>')
+})
+
+server.use((err, req, res, next) => {
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`
+})
+
+console.log(process.env.PORT, process.env.NODE_ENV)
